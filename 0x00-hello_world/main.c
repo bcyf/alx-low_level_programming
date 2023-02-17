1587c7ab76739fa16e4924eb5ff7e175f46bec7e
emmanuel <emmanuelkamara442@gmail.com>
2023-02-17 01:05:40 -0800

    preprocessor

--- 0x00-hello_world/main.c
+++ 0x00-hello_world/main.c
@@ -1,10 +1,10 @@
-#!/bin/bash
 #include <stdio.h>
 /**
  * main - Entry point
  *
- * Return: Always 0 (success)
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-   return (0);
+	return (0);
 }
