834794f234292fde6afc1a1fbe2e7a5dc005c518
emmanuel <emmanuelkamara442@gmail.com>
2023-02-18 01:20:26 -0800

    print task

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - print work
+ * Return: 0
+ */
+int main(void)
+{
+	printf("%s%s%s%s%s", "with proper grammar, ",
+		"but the outcome is a piece of art", ",\n",
+		"", "");
+	return (0);
+}
