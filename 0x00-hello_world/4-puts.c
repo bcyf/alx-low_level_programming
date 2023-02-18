b21e5e35af82b6270812ad7eee51844149f59a30
emmanuel <emmanuelkamara442@gmail.com>
2023-02-18 00:41:01 -0800

    first print

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,10 @@
-#!/bin/bash
-int main(void) {
-    puts("Programming is like building a multilingual puzzle");
-    return 0;
+#include <stdio.h>
+/**
+ * main - first print out
+ *Return: 0
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
