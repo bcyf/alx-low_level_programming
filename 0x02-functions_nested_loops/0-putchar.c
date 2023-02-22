a147d6aa3d6bdba9e1822ccc23a97c783ee4f026
emmanuel <emmanuelkamara442@gmail.com>
2023-02-21 17:39:43 -0800

    put char

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - write the character c to stdout
+ * Return: On success 1.
+ * On error, -1 is returned, and erroe is set appropriately.
+ */
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
